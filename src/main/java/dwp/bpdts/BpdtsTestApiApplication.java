474633ed242f48a553c5f21c8c858934cca61596
nickd <nick.dixon@gmail.com>
2020-06-27 12:32:07 +0100

    Initial check-in
    Commit start.spring.io bundle

---
+++ src/main/java/dwp/bpdts/BpdtsTestApiApplication.java
@@ -0,0 +1,13 @@
+package dwp.bpdts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpdtsTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpdtsTestApiApplication.class, args);
+	}
+
+}
